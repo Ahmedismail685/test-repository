c46f36dab16c3b8e013e47cf56b0b40299330e28
ahmedismail685 <ahmed0128694919@example.com>
2023-05-28 21:05:27 +0300

    test commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router } from 'react-router-dom'
+import AnimatedRoute from "./Page/AnimatedRoute";
+
+function App() {
+
+	return (
+		<Router>
+			<AnimatedRoute />
+		</Router>
+	);
+}
+export default App;
---
+++ src/Page/AnimatedRoute.tsx
@@ -0,0 +1,36 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+
+function AnimatedRoute() {
+  const location = useLocation();
+
+  const PageNotFound = (
+    <motion.h3
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+      className="PageNotFound"
+    >
+      Page Not found <code className="notfound"> 404</code>
+    </motion.h3>
+  );
+
+  return (
+    <Routes location={location} key={location.pathname}>
+      <Route
+        path="/"
+        element={
+          <h2>
+            Helloe in home <span>Page</span>
+          </h2>
+        }
+      />
+      <Route path="/about" element={<h2>Helloe about</h2>} />
+      <Route path="*" element={PageNotFound} />
+    </Routes>
+  );
+}
+
+export default AnimatedRoute;
